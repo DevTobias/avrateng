64fbf547e8e150635e4503c5ecb8172b3f6a4e2a
DevTobias <tobi.kaerst@gmx.de>
2023-07-30 18:15:30 +0200

    feat: fixed style issues

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  safelist: ["row-start-1", "row-start-2", "row-start-3", "row-start-4", "row-start-5", "row-start-6", "row-start-7"],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './lib/**/*.{js,ts,jsx,tsx,mdx}'],
+  safelist: ['row-start-1', 'row-start-2', 'row-start-3', 'row-start-4', 'row-start-5', 'row-start-6', 'row-start-7'],
   theme: { extend: {} },
-  plugins: [require("daisyui")],
-}
+  plugins: [require('daisyui')],
+};
