6b33270cab46f9c0f48bac9337d3eb653c4617cd
DevTobias <tobi.kaerst@gmx.de>
2023-07-30 21:49:05 +0200

    feat: url fix

--- frontend/app/rating/begin/page.tsx
+++ frontend/app/rating/begin/page.tsx
@@ -6,7 +6,7 @@ import { RatingScreen } from '$lib/pages/rating/RatingScreen';
 import { RatingProvider } from '$lib/pages/rating/store/useStore';
 
 export default function Rating() {
-  const { data, error, isLoading } = useSWR('/files', () => fetch('/videos').then((res) => res.json()));
+  const { data, error, isLoading } = useSWR('/files', () => fetch('/files').then((res) => res.json()));
   if (error || isLoading) return <span className='loading loading-dots loading-lg'></span>;
 
   return (
